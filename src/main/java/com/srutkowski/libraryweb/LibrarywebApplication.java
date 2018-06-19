0c06b0fc059446f2360203dee257a915636769d3
sakovski <sakasew@gmail.com>
2018-06-19 22:04:41 +0200

    Added erro handling

--- src/main/java/com/srutkowski/libraryweb/LibrarywebApplication.java
+++ src/main/java/com/srutkowski/libraryweb/LibrarywebApplication.java
@@ -1,9 +1,12 @@
 package com.srutkowski.libraryweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class LibrarywebApplication {
 
     public static void main(String[] args) {
